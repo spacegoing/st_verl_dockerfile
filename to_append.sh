705579622177c7ac2a4431bd72a19f45931f13e5
spacegoing <spacegoing@gmail.com>
2026-01-18 03:48:59 +0000

    [New] vllm012blbharmDEP0118B200.dev

--- to_append.sh
+++ to_append.sh
@@ -12,10 +12,3 @@ tkl() {
 }
 
 alias gd='cd /root/myCodeLab/host/verl'
-
-export http_proxy="http://jdtcom:709a64b73eb3@10.119.176.202:3128"
-export https_proxy="http://jdtcom:709a64b73eb3@10.119.176.202:3128"
-export HTTP_PROXY="http://jdtcom:709a64b73eb3@10.119.176.202:3128"
-export HTTPS_PROXY="http://jdtcom:709a64b73eb3@10.119.176.202:3128"
-export no_proxy="localhost,127.0.0.1"
-export NO_PROXY="localhost,127.0.0.1"
